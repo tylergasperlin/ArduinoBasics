f59c497c16c884f9fef1fe1af24c88b14c26f1ae
tylergasperlin <gasperlint@gmail.com>
2022-10-09 12:21:16 -0700

    Update ledLightButton.ino
    Add notes

--- ledLightButton/ledLightButton.ino
+++ ledLightButton/ledLightButton.ino
@@ -1,6 +1,9 @@
 #define LEDPin 8 
 #define ButtonPin 7
 
+  //output indicates that a signal (or data in other cases) is sent from the system to the LED
+  //input would indicate that a signal (button press) would be received by the system from the device (LED in our case)
+
 void setup() {
   // led pin
   pinMode(LEDPin, OUTPUT);
@@ -9,7 +12,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // if buttonPin's state is LOW it means the button is pressed
   if(digitalRead(ButtonPin)== LOW) {
     // turn LED on
     digitalWrite(LEDPin, HIGH); 
